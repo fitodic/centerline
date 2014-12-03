9c71edbfe0208bf4c4e9654428035bce8f412fcf
fitodic <todic.filip@gmail.com>
2014-12-03 11:03:44 +0100

    Adding centerline files

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-polygon-centerline
+#polygon-centerline
 ==================
 
-Calculates the centerline of a polygon
+##Calculates the centerline of a polygon.
+
+Roads, rivers and similar linear structures are often represented by long and complex polygons. Since one of the most important attributes of a linear structure is its length, extracting that attribute from a polygon can prove to be more or less difficult.
+
+This Python script takes the polygons from a Shapefile, calculates the centerline of each polygon and exports the into a separate Shapefile.
+
+###Usage:
+Before running the script open it and change the name of the input Shapefile (at the bottom of the script). After that, open the Terminal and type:
+'''
+$ python centerline.py
+'''
+
+###Requirements:
+1. [Python 2.7+](https://www.python.org/download/releases/2.7/)
+2. [Shapely](https://pypi.python.org/pypi/Shapely)
+3. [SciPy](http://www.scipy.org/)
+4. [Fiona](https://pypi.python.org/pypi/Fiona/)
+5. [Numpy](http://www.scipy.org/)
+
+###References:
+* [SciPy-Voronoi](http://docs.scipy.org/doc/scipy/reference/tutorial/spatial.html#voronoi-diagrams)
+
+**Notes**:
+When defining the density factor, one has to take into account the coordinate system defined in the Shapefile. The script was designed to handle metric coordinate systems, so the density factor is by default 0.5 (meters).
+
+It appears that the Voronoi function available in the *SciPy* module does not handle large coordinates very well. Since most of the coordinates are large numbers, a bounding box is needed to calculate the minimal X and Y coordinates (bottom left corner). These values are then used for coordinate reduction. Once the Voronoi diagram is created the coordinates are returned to their non-reduced form before creating linestrings.
+
+**Example**
+![Screenshot](Screenshot.png)
---
+++ centerline.py
@@ -0,0 +1,179 @@
+# -*- encoding: utf-8 -*-
+from shapely.geometry import LineString, Polygon, MultiLineString, mapping, shape
+from shapely.ops import unary_union
+from scipy.spatial import Voronoi
+import numpy as np
+import fiona
+
+
+class Centerline(object):
+
+	def __init__(self, inputSHP):
+		self.shp = inputSHP
+
+		print 'Importing Shapefile.'
+		self.dct_polygons = self.importSHP(self.shp)
+
+		print 'Calculating centerlines.'
+		self.dct_centerlines = self.createCenterline(self.dct_polygons)
+
+		print 'Exporting ceterlines to Shapefile.'
+		self.export2SHP(self.dct_centerlines)
+
+
+	def createCenterline(self, dct):
+		"""
+		Calculates the centerline of a polygon.
+
+		Densifies the border of a polygon which is then represented by a Numpy
+		array of points necessary for creating the Voronoi diagram. Once the 
+		diagram is created, the ridges located within the polygon are
+		joined and returned.
+
+		Returns:
+			a union of lines that are located within the polygon.
+		"""
+		dct_clines = {}
+		for i, key in enumerate(dct.keys()):
+		#	set_trace()
+			polygon = dct[key][0]
+			minx = dct[key][1]
+			miny = dct[key][2]
+
+			border = np.array(self.densifyBorder(polygon, minx, miny))
+
+			vor = Voronoi(border)
+			vertex = vor.vertices
+
+			lst_lines = []
+			for j, ridge in enumerate(vor.ridge_vertices):
+				if -1 not in ridge:
+					line = LineString([\
+						(vertex[ridge[0]][0] + minx, vertex[ridge[0]][1] + miny), \
+						(vertex[ridge[1]][0] + minx, vertex[ridge[1]][1] + miny)])
+
+					if line.within(polygon):
+						lst_lines.append(line)
+
+			dct_clines[key] = unary_union(lst_lines)
+
+		return dct_clines
+
+
+	def importSHP(self, shp):
+		""" 
+		Imports the Shapefile into a dictionary.
+		
+		Returns:
+			a dictionary where the ID is the key, and the value is a list
+			that contains the geometry, minimal X and minimal Y coordinate.
+		"""
+
+		dct = {}
+
+		with fiona.open(shp, 'r', encoding = 'UTF-8') as fileIN:
+			for polygon in fileIN:
+				polygonID = polygon['properties'][u'id']
+				geom = shape(polygon['geometry'])
+				minx = int(min(geom.envelope.exterior.xy[0]))
+				miny = int(min(geom.envelope.exterior.xy[1]))
+
+				dct[polygonID] = [geom, minx, miny]
+
+		return dct
+
+
+	def densifyBorder(self, polygon, minx, miny, dist = 0.5):
+		"""
+		Densifies the border of a polygon by a given factor (by default: 0.5).
+
+		The function tests the complexity of the polygons geometry, i.e. does 
+		the polygon have holes or not. If the polygon doesn't have any holes, 
+		its exterior is extracted and densified by a given factor. If the 
+		polygon has holes, the boundary of each hole as well as its exterior is 
+		extracted and densified by a given factor.
+
+		Returns:
+			a list of points where each point is represented by a list of its 
+			reduced coordinates.
+
+		Example:
+			[[X1, Y1], [X2, Y2], ..., [Xn, Yn]
+		"""
+
+		if len(polygon.interiors) == 0:
+			exterIN = LineString(polygon.exterior)
+			points = self.fixedInterpolation(exterIN, minx, miny, dist)
+		
+		else:
+			exterIN = LineString(polygon.exterior)
+			points = self.fixedInterpolation(exterIN, minx, miny, dist)
+
+			for j in range(len(polygon.interiors)):
+				interIN = LineString(polygon.interiors[j])
+				points += self.fixedInterpolation(interIN, minx, miny, dist)
+
+		return points
+
+
+	def fixedInterpolation(self, line, minx, miny, dist):
+		"""
+		A helping function which is used in densifying the border of a polygon.
+
+		It places points on the border at the specified distance. By default the
+		distance is 0.5 (meters) which means that the first point will be placed
+		0.5 m from the starting point, the second point will be placed at the 
+		distance of 1.0 m from the first point, etc. Naturally, the loop breaks 
+		when the summarized distance exceeds the length of the line.
+
+		Returns:
+			a list of points where each point is represented by a list of its 
+			reduced coordinates.
+
+		Example:
+			[[X1, Y1], [X2, Y2], ..., [Xn, Yn]
+		"""
+
+		count = dist
+		newline = []
+
+		startpoint = [line.xy[0][0] - minx, line.xy[1][0] - miny]
+		endpoint = [line.xy[0][-1] - minx, line.xy[1][-1] - miny]
+		newline.append(startpoint)
+
+		while count < line.length:
+			point = line.interpolate(count)
+			newline.append([point.x - minx, point.y - miny])
+			count += dist
+
+		newline.append(endpoint)
+
+		return newline
+
+
+	def export2SHP(self, dct):
+		"""
+		Creates a Shapefile and fills it centerlines and their IDs.
+
+		The dictionary contains the IDs of the centerlines (keys) and their 
+		geometries (values). The ID of a centerline is the same as the ID of 
+		the polygon it represents.
+		"""
+
+		newschema = {'geometry': 'MultiLineString', 'properties': {'id': 'int'}}
+
+		with fiona.open('centerlines.shp', 'w', encoding='UTF-8', \
+			schema = newschema, driver = 'ESRI Shapefile') as SHPfile:
+			
+			for i, key in enumerate(dct):
+				geom = dct[key]
+				newline = {}
+
+				newline['geometry'] = mapping(geom)
+				newline['properties'] = {'id': key}
+
+				SHPfile.write(newline)
+
+
+if __name__ == "__main__":
+	Centerline('inputSHP.shp')
