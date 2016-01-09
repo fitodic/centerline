3216abb8b2478ded8c7bb9e629204cd8c37d1995
fitodic <todic.filip@gmail.com>
2016-01-09 20:28:32 +0100

    Create basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='centerline',
+      version='0.1',
+      description='Calculate the centerline of a polygon',
+      url='https://github.com/fitodic/centerline.git',
+      author='Filip Todic',
+      author_email='todic.filip@gmail.com',
+      license='MIT',
+      packages=['centerline'],
+      zip_safe=False)
