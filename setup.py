a44f5d95681917ba87e81363e2adf58c332019f1
fitodic <todic.filip@gmail.com>
2016-01-10 17:12:19 +0100

    Add package metadata and conversion from MD to RST

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(name='centerline',
       version='0.1',
       description='Calculate the centerline of a polygon',
+      long_description=read_md('README.md'),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+      ],
       url='https://github.com/fitodic/centerline.git',
       author='Filip Todic',
       author_email='todic.filip@gmail.com',
       license='MIT',
       packages=['centerline'],
+      install_requires=['numpy', 'scipy', 'Shapely', 'Fiona'],
+      include_package_data=True,
       zip_safe=False)
