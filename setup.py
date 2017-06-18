eb10e9b5b0397a6bae08b68212046deb63252c44
fitodic <todic.filip@gmail.com>
2017-06-18 12:42:03 +0200

    Define the primary requirements

--- setup.py
+++ setup.py
@@ -14,23 +14,33 @@ except ImportError:
     def read_md():
         return lambda f: open(f, 'r').read()
 
-setup(name='centerline',
-      version='0.1',
-      description='Calculate the centerline of a polygon',
-      long_description=read_md('README.md'),
-      classifiers=[
+setup(
+    name='centerline',
+    version='0.1',
+    description='Calculate the centerline of a polygon',
+    long_description=read_md('README.md'),
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: GIS'
-      ],
-      url='https://github.com/fitodic/centerline.git',
-      author='Filip Todic',
-      author_email='todic.filip@gmail.com',
-      license='MIT',
-      packages=['centerline'],
-      install_requires=['numpy', 'scipy', 'Shapely', 'GDAL', 'click', 'cligj', 'six', 'Fiona'],
-      scripts=['bin/shp2centerline'],
-      include_package_data=True,
-      zip_safe=False)
+    ],
+    url='https://github.com/fitodic/centerline.git',
+    author='Filip Todic',
+    author_email='todic.filip@gmail.com',
+    license='MIT',
+    packages=['centerline'],
+    install_requires=[
+        'GDAL>=2.0.1',
+        'Fiona>=1.6.3'
+        'Shapely>=1.5.13',
+        'numpy>=1.10.4',
+        'scipy>=0.16.1',
+    ],
+    scripts=[
+        'bin/shp2centerline',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
