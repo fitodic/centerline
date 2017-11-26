869f424be9c712e708bb12eb16ecf81abedc8c8d
fitodic <todic.filip@gmail.com>
2017-11-26 10:30:01 +0100

    Revert "Remove utility modules"
    This reverts commit a218684675e72ed8969a17383d695724e7f620c4.

---
+++ centerline/utils.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+
+from osgeo import gdal, ogr
+
+# Enable GDAL/OGR exceptions
+gdal.UseExceptions()
+
+
+def get_spatial_reference_from_layer(layer):
+    """Get the layer's spatial reference.
+
+    Args:
+        layer {osgeo.ogr.Layer}: layer
+
+    Returns:
+        {osgeo.osr.SpatialReference}
+
+    """
+    return layer.GetSpatialRef()
+
+
+def get_ogr_driver(filepath):
+    """Get the OGR driver from the provided file extension.
+
+    Args:
+        file_extension {str}: file extension
+
+    Returns:
+        {osgeo.ogr.Driver}
+
+    Raises:
+        {ValueError}: when no driver is found
+
+    """
+    filename, file_extension = os.path.splitext(filepath)
+    EXTENSION = file_extension[1:]
+
+    ogr_driver_count = ogr.GetDriverCount()
+    for idx in range(ogr_driver_count):
+        driver = ogr.GetDriver(idx)
+        driver_extension = driver.GetMetadataItem(str('DMD_EXTENSION')) or ''
+        driver_extensions = driver.GetMetadataItem(str('DMD_EXTENSIONS')) or ''
+
+        if EXTENSION == driver_extension or EXTENSION in driver_extensions:
+            return driver
+
+    else:
+        msg = 'No driver found for the following file extension: {}'.format(
+            EXTENSION)
+        raise ValueError(msg)
+
+
+def get_polygon_features(filepath):
+    """Get the Polygon or MultiPolygon features from the source file.
+
+    Args:
+        filepath {str}: path to the source file
+
+    Yields:
+        {osgeo.ogr.Feature}: a Polygon or a MultiPolygon feature
+
+    Raises:
+        {AttributeError}: invalid filepath
+        {ValueError}: no driver is found
+
+    """
+    try:
+        driver = get_ogr_driver(filepath=filepath)
+    except ValueError:
+        raise
+
+    datasource = driver.Open(filepath, 0)
+
+    try:
+        layer_count = datasource.GetLayerCount()
+    except AttributeError:
+        raise
+
+    for layer_idx in range(layer_count):
+        layer = datasource.GetLayerByIndex(layer_idx)
+        layer_geometry_type = layer.GetGeomType()
+
+        if layer_geometry_type == ogr.wkbPolygon:
+            feature = layer.GetNextFeature()
+            while feature is not None:
+                yield feature
+                feature = layer.GetNextFeature()
+
+    datasource.Destroy()
---
+++ tests/test_utils.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+from unittest import TestCase
+
+from centerline.utils import (get_ogr_driver, get_polygon_features,
+                              get_spatial_reference_from_layer, save_features)
+from osgeo import ogr
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+SHP_DIR = os.path.join(TESTS_DIR, 'data', 'shp')
+GEOJSON_DIR = os.path.join(TESTS_DIR, 'data', 'geojson')
+
+
+class TestGetOgrDriver(TestCase):
+
+    def test__driver_name__with_shp__returns_esri_shapefile(self):
+        FILE_EXTENSION = 'example.shp'
+        EXPECTED_DRIVER_NAME = 'ESRI Shapefile'
+
+        driver = get_ogr_driver(FILE_EXTENSION)
+
+        self.assertEqual(driver.GetName(), EXPECTED_DRIVER_NAME)
+
+    def test__driver_name__with_json__returns_geojson(self):
+        FILE_EXTENSION = 'example.json'
+        EXPECTED_DRIVER_NAME = 'GeoJSON'
+
+        driver = get_ogr_driver(FILE_EXTENSION)
+
+        self.assertEqual(driver.GetName(), EXPECTED_DRIVER_NAME)
+
+    def test__driver_name__with_geojson__returns_geojson(self):
+        FILE_EXTENSION = 'example.geojson'
+        EXPECTED_DRIVER_NAME = 'GeoJSON'
+
+        driver = get_ogr_driver(FILE_EXTENSION)
+
+        self.assertEqual(driver.GetName(), EXPECTED_DRIVER_NAME)
+
+    def test__with_unknown_extension__returns_valueerror(self):
+        FILE_EXTENSION = 'example.unknown'
+
+        with self.assertRaises(ValueError):
+            get_ogr_driver(FILE_EXTENSION)
+
+
+class TestGetPolygonFeature(TestCase):
+
+    def test__shp__polygons__feature_count(self):
+        EXPECTED_FEATURE_COUNT = 3
+
+        FILEPATH = os.path.join(SHP_DIR, 'polygons.shp')
+
+        features = [feature for feature in get_polygon_features(FILEPATH)]
+
+        self.assertEqual(len(features), EXPECTED_FEATURE_COUNT)
+
+    def test__shp__polygons__feature_type__returns_ogr_feature(self):
+        FILEPATH = os.path.join(SHP_DIR, 'polygons.shp')
+
+        for feature in get_polygon_features(FILEPATH):
+            self.assertIsInstance(feature, ogr.Feature)
+
+    def test__geojson__polygons__feature_count(self):
+        EXPECTED_FEATURE_COUNT = 3
+
+        FILEPATH = os.path.join(GEOJSON_DIR, 'polygons.geojson')
+
+        features = [feature for feature in get_polygon_features(FILEPATH)]
+
+        self.assertEqual(len(features), EXPECTED_FEATURE_COUNT)
+
+    def test__geojson__polygons__feature_type__returns_ogr_feature(self):
+        FILEPATH = os.path.join(SHP_DIR, 'polygons.shp')
+
+        for feature in get_polygon_features(FILEPATH):
+            self.assertIsInstance(feature, ogr.Feature)
+
+    def test__geojson__linestrings__feature_count(self):
+        EXPECTED_FEATURE_COUNT = 0
+
+        FILEPATH = os.path.join(GEOJSON_DIR, 'linestrings.geojson')
+
+        features = [feature for feature in get_polygon_features(FILEPATH)]
+
+        self.assertEqual(len(features), EXPECTED_FEATURE_COUNT)
+
+    def test__geojson__points__feature_count(self):
+        EXPECTED_FEATURE_COUNT = 0
+
+        FILEPATH = os.path.join(GEOJSON_DIR, 'points.geojson')
+
+        features = [feature for feature in get_polygon_features(FILEPATH)]
+
+        self.assertEqual(len(features), EXPECTED_FEATURE_COUNT)
+
+    def test__nonexistent_file_with_valid_extension(self):
+        FILEPATH = os.path.join(GEOJSON_DIR, 'nonexistent.geojson')
+
+        with self.assertRaises(AttributeError):
+            [feature for feature in get_polygon_features(FILEPATH)]
+
+    def test__file_with_an_invalid_extension(self):
+        FILEPATH = os.path.join(GEOJSON_DIR, 'nonexistent.invalid')
+
+        with self.assertRaises(ValueError):
+            [feature for feature in get_polygon_features(FILEPATH)]
+
+
+class TestGetSpatialReferenceFromLayer(TestCase):
+
+    def test__shapefile_spatial_ref(self):
+        EXPECTED_OUTPUT_PROJ4 = '+proj=longlat +datum=WGS84 +no_defs '
+        FILEPATH = os.path.join(SHP_DIR, 'polygons.shp')
+
+        driver = get_ogr_driver(filepath=FILEPATH)
+        datasource = driver.Open(FILEPATH, 0)
+        layer = datasource.GetLayer()
+
+        spatial_ref = get_spatial_reference_from_layer(layer)
+
+        self.assertEqual(spatial_ref.ExportToProj4(), EXPECTED_OUTPUT_PROJ4)
+
+    def test__geojson_spatial_ref(self):
+        EXPECTED_OUTPUT_PROJ4 = '+proj=longlat +datum=WGS84 +no_defs '
+        FILEPATH = os.path.join(GEOJSON_DIR, 'polygons.geojson')
+
+        driver = get_ogr_driver(filepath=FILEPATH)
+        datasource = driver.Open(FILEPATH, 0)
+        layer = datasource.GetLayer()
+
+        spatial_ref = get_spatial_reference_from_layer(layer)
+
+        self.assertEqual(spatial_ref.ExportToProj4(), EXPECTED_OUTPUT_PROJ4)
+
