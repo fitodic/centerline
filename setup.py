e6eee5e598490d2e391fcd618e4c77aef6f83a01
fitodic <todic.filip@gmail.com>
2017-11-26 16:11:00 +0100

    Bump the package version to 0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='centerline',
-    version='0.2.1',
+    version='0.3',
     description='Calculate the centerline of a polygon',
     long_description=long_description,
     classifiers=[
